74aad44c1b4e255f315cee59eaed5cf1e47dab57
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-08-21 20:59:48 -0400

    Completed lecture 14

--- follow-along/myapp/src/App.js
+++ follow-along/myapp/src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas'
 
 class App extends Component {
+  state = {
+    ninjas: [
+        { name: "Mose", age: "1.5", belt: "black", id: 1 },
+        { name: "Ripley", age: "3", belt: "brown", id: 2 },
+        { name: "Vero", age: "35", belt: "yello", id: 3 }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome :D</p>
-        <Ninjas name="Mose" age="1.5" belt="black"/>
-        <Ninjas name="Ripley" age="3" belt="brown"/>
-        <Ninjas name="Vero" age="35" belt="yello"/>
+        <Ninjas ninjas = { this.state.ninjas } />
       </div>
     );
   }
