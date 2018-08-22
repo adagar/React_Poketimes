b9060e6591d155ae02d7daaa666aaa92faac47c4
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-08-21 20:21:56 -0400

    Completed lecture 12

---
+++ follow-along/myapp/src/Ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component{
+    render() {
+        return(
+            <div className="ninja">
+                <div>Name: Ripley</div>
+                <div>Age: 30</div>
+                <div>Belt: Brown</div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas
