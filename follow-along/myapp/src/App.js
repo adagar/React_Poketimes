adf8eed5ff666f1ed1e8f0fca48d39349574d734
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-08-21 20:37:27 -0400

    completed lecture 12

--- follow-along/myapp/src/App.js
+++ follow-along/myapp/src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome :D</p>
-        <Ninjas />
+        <Ninjas name="Mose" age="1.5" belt="black"/>
+        <Ninjas name="Ripley" age="3" belt="brown"/>
+        <Ninjas name="Vero" age="35" belt="yello"/>
       </div>
     );
   }
--- follow-along/myapp/src/Ninjas.js
+++ follow-along/myapp/src/Ninjas.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 class Ninjas extends Component{
     render() {
+        //console.log(this.props)
+        const { name, age, belt } = this.props;
         return(
             <div className="ninja">
-                <div>Name: Ripley</div>
-                <div>Age: 30</div>
-                <div>Belt: Brown</div>
+                <div>Name: { name }</div>
+                <div>Age: { age }</div>
+                <div>Belt: { belt }</div>
             </div>
         )
     }
