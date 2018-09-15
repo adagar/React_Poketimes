0e5d77892d6713e96e80c0a4cfa9df9053a28e8b
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-09-15 11:04:35 -0400

    Completed video 29

--- follow-along/poketimes/src/components/About.js
+++ follow-along/poketimes/src/components/About.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
+import Rainbow from "../hoc/Rainbow";
 
 const About = () => {
-    return (
-        <div className="container">
-            <h4 className="center">About</h4>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est accusamus, a laboriosam deserunt optio aperiam laborum, unde illum suscipit corporis, quos dicta. Enim ea provident dolores! Consequatur tenetur ipsam perferendis?</p>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est accusamus,
+        a laboriosam deserunt optio aperiam laborum, unde illum suscipit
+        corporis, quos dicta. Enim ea provident dolores! Consequatur tenetur
+        ipsam perferendis?
+      </p>
+    </div>
+  );
+};
 
-export default About;
+export default Rainbow(About);
